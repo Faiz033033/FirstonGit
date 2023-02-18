fb9340d218af8960849724377b44df09b9f04a30
faiz033033 <feju302@gmail.com>
2023-02-18 12:14:11 +0530

    second commit

---
+++ doc.js
@@ -0,0 +1,7 @@
+//console.log(document.getElementById('header-title'));
+var faiz = document.getElementById('header-title');
+console.log(faiz)
+faiz.style.borderBottom ='solid';
+var faiz1 = document.getElementById('main');
+console.log(faiz1)
+faiz1.style="color:green"
