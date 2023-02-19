e9680304f4521f8c5e081279863be7a030b91285
faiz033033 <feju302@gmail.com>
2023-02-19 12:59:12 +0530

    GetElementsbyTAgName

--- doc.js
+++ doc.js
@@ -5,14 +5,20 @@
 //var faiz1 = document.getElementById('main');
 //console.log(faiz1)
 //faiz1.style="color:green"
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
 
-items[1].style.backgroundColor='green';
+//items[1].style.backgroundColor='green';
 
-for(var i = 0; i < items.length; i++){
-    items[i].style.fontWeight='bold';
+//for(var i = 0; i < items.length; i++){
+ //   items[i].style.fontWeight='bold';
 
-}
+//}
+var li = document.getElementsByTagName('li');
+console.log(li);
+li[1].style.backgroundColor='red'
+
+var input = document.querySelector('input');
+input.value = 'Please Enter Here'
 
