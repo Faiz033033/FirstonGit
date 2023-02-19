800aef832e1b76963090a7729f934344e05fae98
faiz033033 <feju302@gmail.com>
2023-02-19 12:40:29 +0530

    GetElementsbyClassname

--- doc.js
+++ doc.js
@@ -1,7 +1,18 @@
 //console.log(document.getElementById('header-title'));
-var faiz = document.getElementById('header-title');
-console.log(faiz)
-faiz.style.borderBottom ='solid';
-var faiz1 = document.getElementById('main');
-console.log(faiz1)
-faiz1.style="color:green"
+//var faiz = document.getElementById('header-title');
+//console.log(faiz)
+//faiz.style.borderBottom ='solid';
+//var faiz1 = document.getElementById('main');
+//console.log(faiz1)
+//faiz1.style="color:green"
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+
+items[1].style.backgroundColor='green';
+
+for(var i = 0; i < items.length; i++){
+    items[i].style.fontWeight='bold';
+
+}
+
