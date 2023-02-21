95a106f6cdf617cff613613a3bebced389879c6d
faiz033033 <feju302@gmail.com>
2023-02-22 00:03:47 +0530

    Creating Nodes and Modifying Dom

--- doc.js
+++ doc.js
@@ -27,12 +27,41 @@ var input = document.querySelector('input');
 input.value = 'Please Enter Here'
 console.log(input); */
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.Color = 'green';
+//var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.Color = 'green';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0; i < odd.length; i++){
+//var odd = document.querySelectorAll('li:nth-child(odd)');
+/* for(var i=0; i < odd.length; i++){
     odd[i].style.backgroundColor = 'green';
 }
+ */
+// var itemList = document.querySelector('#items');
+// // console.log(itemList.parentNode);
+// // var abcd = itemList.parentNode;
+// // abcd.style.backgroundColor = 'gray';
+// console.log(itemList.children);
+// console.log(itemList.children[2]);
+// itemList.children[2].style.backgroundColor='green';
+
+// var itemList = document.querySelector('#header-titlr');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'red';
+
+var newDiv = document.createElement('div');
+newDiv.className = 'hey';
+newDiv.id = 'hey1';
+
+var newDivtext = document.createTextNode('Hello');
+
+newDiv.appendChild(newDivtext);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+container.insertBefore(newDiv, h1);
+
+
+
+
 
 
